6c1822aee028242f19b5034122c87cba12ba8654
JuanAndresM23 <juanandresmunozochoa@gmail.com>
2026-03-27 21:02:16 -0500

    petlist update

---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+AppRegistry.registerComponent(appName, () => App);
